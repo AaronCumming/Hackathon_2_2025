54dcfb39fb8b160ac25eb01fe40ca35a24f004d2
Aaron Cumming <aaroncumming04@gmail.com>
2025-02-22 17:07:34 -0600

    tweaked home and allauth

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,3 +134,9 @@ AUTHENTICATION_BACKENDS = [
     # `allauth` specific authentication methods, such as login by email
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
+
+
+LOGIN_REDIRECT_URL = reverse_lazy("cune_connect:home")
+ACCOUNT_LOGOUT_REDIRECT_URL = reverse_lazy("cune_connect:home")
+ACCOUNT_SIGNUP_REDIRECT_URL = reverse_lazy("cune_connect:home")
+
