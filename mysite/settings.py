5caef3923a680c28f30198b7f3d7b69960c27383
Aaron Cumming <aaroncumming04@gmail.com>
2025-02-22 15:59:14 -0600

    pretty good home page

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    #'cune_connect.apps.CuneConnectConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
